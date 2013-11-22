0d1f7c502b7c8bf6dded09522831059db2939400
Scott Nelson <scott@scttnlsn.com>
2013-11-22 11:55:10 -0500

    Initial commit

---
+++ test/test_matcher.js
@@ -0,0 +1,143 @@
+var assert = require('assert');
+var equals = require('equals');
+var Matcher = require('router/lib/matcher');
+
+describe('Matcher', function () {
+    beforeEach(function () {
+        this.matcher = new Matcher();
+    });
+
+    it('matches static routes', function () {
+        var h = {};
+        this.matcher.add([{ path: '/foo/bar', handler: h }]);
+
+        var params = this.matcher.match('/foo/bar');
+        assert(equals(params, [{ handler: h, params: {} }]));
+        assert(this.matcher.match('/foo/baz') === null);
+    });
+
+    it('matches dynamic routes', function () {
+        var h = {};
+        this.matcher.add([{ path: '/foo/:bar', handler: h }]);
+
+        var match = this.matcher.match('/foo/123');
+        assert(equals(match, [{ handler: h, params: { bar: '123' }}]));
+        assert(this.matcher.match('/bar') === null);
+    });
+
+    it('matches wildcard routes', function () {
+        var h = {};
+        this.matcher.add([{ path: '/foo/*bar', handler: h }]);
+
+        var match = this.matcher.match('/foo/1/2/3');
+        assert(equals(match, [{ handler: h, params: { bar: '1/2/3' }}]));
+    });
+
+    it('matches against multiple routes', function () {
+        var h = [{}, {}];
+
+        this.matcher.add([{ path: '/foo/:bar', handler: h[0] }]);
+        this.matcher.add([{ path: '/baz/:qux', handler: h[1] }]);
+
+        var m1 = this.matcher.match('/foo/123');
+        assert(equals(m1, [{ handler: h[0], params: { bar: '123' }}]));
+        var m2 = this.matcher.match('/baz/456');
+        assert(equals(m2, [{ handler: h[1], params: { qux: '456' }}]));
+    });
+
+    it('matches overlapping routes', function () {
+        var h = [{}, {}];
+
+        this.matcher.add([{ path: '/foo/:bar', handler: h[0] }]);
+        this.matcher.add([{ path: '/foo/bar/:baz', handler: h[1] }]);
+
+        var m1 = this.matcher.match('/foo/123');
+        assert(equals(m1, [{ handler: h[0], params: { bar: '123' }}]));
+        var m2 = this.matcher.match('/foo/bar/456');
+        assert(equals(m2, [{ handler: h[1], params: { baz: '456' }}]));
+    });
+
+    it('matches nested routes', function () {
+        var h = [{}, {}];
+
+        this.matcher.add([
+            { path: '/foo/:bar', handler: h[0] },
+            { path: '/baz/:qux', handler: h[1] }
+        ]);
+
+        var match = this.matcher.match('/foo/123/baz/456');
+        assert(equals(match, [
+            { handler: h[0], params: { bar: '123' } },
+            { handler: h[1], params: { qux: '456' } }
+        ]));
+    });
+
+    it('matches empty routes', function () {
+        var h = [{}, {}, {}, {}];
+
+        this.matcher.add([
+            { path: '/foo', handler: h[0] },
+            { path: '/', handler: h[1] },
+            { path: '/bar', handler: h[2] }
+        ]);
+
+        this.matcher.add([
+            { path: '/foo', handler: h[0] },
+            { path: '/', handler: h[1] },
+            { path: '/:bar', handler: h[3] }
+        ]);
+
+        var m1 = this.matcher.match('/foo/bar');
+        assert(equals(m1, [
+            { handler: h[0], params: {} },
+            { handler: h[1], params: {} },
+            { handler: h[2], params: {} }
+        ]));
+
+        var m2 = this.matcher.match('/foo/baz');
+        assert(equals(m2, [
+            { handler: h[0], params: {} },
+            { handler: h[1], params: {} },
+            { handler: h[3], params: { bar: 'baz' }}
+        ]));
+    });
+
+    it('gives preference to routes with fewer dynamic segments', function () {
+        var h = [{}, {}];
+
+        this.matcher.add([{ path: '/foo/:bar', handler: h[0] }]);
+        this.matcher.add([{ path: '/foo/bar', handler: h[1] }]);
+
+        var m1 = this.matcher.match('/foo/bar');
+        assert(equals(m1, [{ handler: h[1], params: {}}]));
+        var m2 = this.matcher.match('/foo/baz');
+        assert(equals(m2, [{ handler: h[0], params: { bar: 'baz' }}]));
+    });
+
+    describe('', function () {
+        beforeEach(function () {
+            var h = this.h = [{}, {}, {}];
+
+            this.matcher.add([{ path: '/foo', handler: h[0] }], { name: 'a' });
+            this.matcher.add([{ path: '/foo/:bar', handler: h[1] }], { name: 'b' });
+            this.matcher.add([{ path: '/foo', handler: h[0] }, { path: '/bar', handler: h[2] }], { name: 'c' });
+        });
+
+        it('returns paths for named routes', function () {
+            assert(this.matcher.generate('a') === '/foo');
+            assert(this.matcher.generate('b', { bar: '123' }) === '/foo/123');
+            assert(this.matcher.generate('c') === '/foo/bar');
+        });
+
+        it('returns handlers for named routes', function () {
+            var h1 = this.matcher.handlers('a');
+            assert(equals(h1, [{ handler: this.h[0], names: [] }]));
+
+            var h2 = this.matcher.handlers('b');
+            assert(equals(h2, [{ handler: this.h[1], names: ['bar'] }]));
+
+            var h3 = this.matcher.handlers('c');
+            assert(equals(h3, [{ handler: this.h[0], names: [] }, { handler: this.h[2], names: [] }]));
+        });
+    });
+});
