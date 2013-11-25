df428df808ef502750671471863a1f738f0e6bb2
Scott Nelson <scott@scttnlsn.com>
2013-11-25 13:36:51 -0500

    Added simple example

---
+++ example/index.js
@@ -0,0 +1,85 @@
+var Router = require('router');
+
+$(function () {
+    var router = Router();
+
+    var templates = {
+        index: _.template($('#index-template').html()),
+        entries: _.template($('#entries-template').html()),
+        entry: _.template($('#entry-template').html())
+    };
+
+    var handlers = {
+        index: {
+            enter: function () {
+                console.log('Enter:', 'index');
+            },
+
+            exec: function () {
+                console.log('Exec:', 'index');
+                $('#main').html(templates.index());
+            },
+
+            exit: function () {
+                console.log('Exit:', 'index');
+            }
+        },
+
+        entries: {
+            enter: function () {
+                console.log('Enter:', 'entries');
+
+                this.entries = {
+                    '1': { id: 1, title: 'Foo', body: 'This is entry foo.' },
+                    '2': { id: 2, title: 'Bar', body: 'This is entry bar.' }
+                };
+            },
+
+            exec: function () {
+                console.log('Exec:', 'entries');
+                $('#main').html(templates.entries({ entries: this.entries }));
+            },
+
+            exit: function () {
+                console.log('Exit:', 'entries');
+            }
+        },
+
+        entry: {
+            enter: function () {
+                console.log('Enter:', 'entry', this.params.id);
+                this.entry = this.entries[this.params.id];
+            },
+
+            exec: function () {
+                console.log('Exec:', 'entry', this.params.id);
+                $('#main').html(templates.entry({ entry: this.entry }));
+            },
+
+            exit: function () {
+                console.log('Exit:', 'entry', this.params.id);
+            }
+        }
+    };
+
+    router.define(function (route) {
+        route('/').to(handlers.index, function (route) {
+            route('/entries').to(handlers.entries, function (route) {
+                route('/:id').to(handlers.entry);
+            });
+        });
+    });
+
+    window.onhashchange = function () {
+        router.navigate(path());
+    };
+
+    router.navigate(path());
+
+    function path() {
+        var path = window.location.hash;
+        if (path[0] === '#') path = path.slice(1);
+        if (path === '') path = '/';
+        return path;
+    }
+});
